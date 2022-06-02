cd0c3301c55d2ab79b858167d2359710ba8111e8
Alex Matte <alex.matte71@gmail.com>
2022-06-02 16:34:56 +0200

    Created basic function that: add, subtract, multiple and divide.

---
+++ script.js
@@ -0,0 +1,24 @@
+//1) create functions that take two numbers as args and add, subtract, multiply or divide them.
+function add(num1, num2){
+    return num1 + num2;
+};
+
+function subtract(num1, num2){
+    return num1 - num2;
+};
+
+function multiply(num1, num2){
+    return num1 * num2;
+};
+
+function divide(num1, num2){
+    return num1 / num2;
+};
+
+//2) create a new operate function that takes 3 args. An operator and two numbers. This function will then evoke functions
+//that were created in step 1.
+
+function operate (operator, num1, num2){
+    
+
+};
