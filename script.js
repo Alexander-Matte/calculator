f4c23c1bf4904e3180cb4dab0ccff3573c4027d6
Alex Matte <alex.matte71@gmail.com>
2022-06-03 20:47:41 +0200

    Added eventlistener to every div button in calculator

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// 4)create a display field and fill it with some dummy numbers to get it looking good.
+// 5) create a function that will populate the display when clicking number buttons.
+let displayField = document.querySelector(".mainDisplay");
+let divs = document.querySelectorAll(".btn");
+let displayNumber;
+
+
+// 7)When the user presses an operator, you have to store the first value and which operator was selected. Lastly you have to run the 
+//operate() function when user presses the equals (=) key.
+
+
+//create array from array like node list
+divsArray = Array.from(divs);
+
+divsArray.forEach((el) => {
+  // listen for a click and save text content as value
+  el.addEventListener("click", (event) => {
+    displayField.textContent= event.target.textContent;
+  })
+});
+
 
 //1) create functions that take two numbers as args and add, subtract, multiply or divide them.
 function add(num1, num2){
@@ -57,3 +78,5 @@ function operate (operator, num1, num2){
       };
       
 };
+
+
