3fd4dfdbb9d9f3328c037210fc9a9e900d07ab0e
Alex Matte <alex.matte71@gmail.com>
2022-06-17 13:48:43 +0200

    >Created runningTotal array. >Reworked conditional statement by using a boolean value. >Looped over individual sections of calculator. >Created percent function.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // 4)create a display field and fill it with some dummy numbers to get it looking good.
 // 5) create a function that will populate the display when clicking number buttons.
 let displayField = document.querySelector(".mainDisplay");
-let divs = document.querySelectorAll(".btn");
-let displayNumber;
+let numDivs = document.querySelectorAll(".num");
+let opDivs = document.querySelectorAll(".op");
+let eqlBtn = document.querySelector(".equalBtn");
+let updatingDisplay = document.querySelector(".updatingDisplay");
+let clrBtn = document.querySelector(".clear");
 
 let values = {
   "value1": "",
@@ -10,70 +13,137 @@ let values = {
   "value2": ""
 };
 
-
-// 7)When the user presses an operator, you have to store the first value and which operator was selected. Lastly you have to run the 
-//operate() function when user presses the equals (=) key.
+let runningTotal = [];
 
 
-//create array from array like node list
-divsArray = Array.from(divs);
 
-divsArray.forEach((el) => {
-  // listen for a click and save text content as value
-  el.addEventListener("click", (event) => {
-    displayField.textContent = event.target.textContent;
- 
-    
-    if((values.value1 !== "") && (values.operator !== "") && (event.target.className === "btn num")){
+/*     Created a boolean,and put its value to false.
+Looped over the numbers
+If, the boolean value is true, store the number in the second variable
+Else, store the number in the first variable. (Since the boolean will 
+remain false for the first variable, the number is going to store in the first variable by default) */
+hasValues = false;
+
+numArray = Array.from(numDivs);
+opArray = Array.from(opDivs);
+// Adds eventlistener to each numbers button and saves values in object
+numArray.forEach(Element => {
+  Element.addEventListener("click", (event) => {
+
+    if(hasValues){
       values.value2 += event.target.textContent;
-    }else if((values.value1 !== "") && (event.target.className === "btn")){
-      values.operator = event.target.textContent;
-    }else if(event.target.className === "btn num") {
+      displayField.textContent = values.value1 + " " + values.operator + " " + values.value2;
+    }else {
       values.value1 += event.target.textContent;
-      
+      displayField.textContent = values.value1 + " " + values.operator + " " + values.value2;
     };
-
-    if((event.target.className === "btn equalBtn") && (values.value1 !== "") && (values.value2 !== "") && (values.operator !== "")) {
-      operate(values.operator, Number(values.value1), Number(values.value2));
-      clear(values);
-    };
-
-
-
     console.log(values);
-
-
-
-
-
+    console.log(runningTotal);
   });
 });
+//Adds eventlistener to ever operator button and performs operations simultaneously 
+opArray.forEach(element => {
+  element.addEventListener("click", (event) => {
+    if (!hasValues) {
+      values.operator = event.target.textContent;
+      displayField.textContent = values.value1 + " " + values.operator + " " + values.value2;
+      hasValues = true;
+
+    } else {
+      operate(values.operator, Number(values.value1), Number(values.value2));
+      displayField.textContent = values.value1;
+      values.operator = event.target.textContent;
+      updatingDisplay.textContent = values.operator;
+    };
+    console.log(values);
+    console.log(runningTotal);
+  }) 
+});
+//Listens for click on equal button.
+eqlBtn.addEventListener("click", (event) => {
+  if (hasValues && (values.value2 !== "")) {
+    operate(values.operator, Number(values.value1), Number(values.value2));
+  } else {
+    displayField.textContent = values.value1;
+  };
+  updatingDisplay.textContent = "";
+  console.log(values);
+  console.log(runningTotal);
+});
+//Listens for click on Clear button,
+clrBtn.addEventListener("click", (event) => {
+  clear(values);
+  displayField.textContent = "";
+  runningTotal = [];
+  hasValues = false;
+  updatingDisplay.textContent = "";
+  console.log(values);
+  console.log(runningTotal);
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 //1) create functions that take two numbers as args and add, subtract, multiply or divide them.
 function add(num1, num2){
     let result = num1 + num2;
     console.log(result);
-    return result;
+    runningTotal.push(result);
+    clear(values);
+    values.value1 = result;
+    hasValues = true;
+    displayField.textContent = result;
 };
 
 function subtract(num1, num2){
     let result =  num1 - num2;
     console.log(result);
-    return result;
+    runningTotal.push(result);
+    clear(values);
+    values.value1 = result;
+    hasValues = true;
+    displayField.textContent = result;
 };
 
 function multiply(num1, num2){
     let result =  num1 * num2;
     console.log(result);
-    return result;
+    runningTotal.push(result);
+    clear(values);
+    values.value1 = result;
+    hasValues = true;
+    displayField.textContent = result;
 };
 
 function divide(num1, num2){
     let result =  num1 / num2;
     console.log(result);
-    return result;
+    runningTotal.push(result);
+    clear(values);
+    values.value1 = result;
+    hasValues = true;
+    displayField.textContent = result;
 };
+
+function percentage(num1, num2){
+    let result = (num1/ 100) * num2;
+    console.log(result);
+    runningTotal.push(result);
+    clear(values);
+    values.value1 = result;
+    hasValues = true;
+    displayField.textContent = result;
+};
+
 
 //2) create a new operate function that takes 3 args. An operator and two numbers. This function will then evoke functions
 //that were created in step 1.
@@ -99,13 +169,13 @@ function operate (operator, num1, num2){
           //Statements executed when the
           //result of expression matches "/"
           divide(num1, num2);
-        break;
-        default:
-          //Statements executed when none of
-          //the values match the value of the expression
-          alert("Not a valid operator!");
           break;
-      };
+        case "%":
+          //Statements executed when the
+          //result of expression matches "%"
+          percentage(num1, num2);
+          break;
+    };
       
 };
 
